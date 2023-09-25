e96bdcd4f2439befab1cbe6b5b493c614fe124a9
Raahim Zeeshan <raahim.home@gmail.com>
2023-09-25 23:33:10 +0500

    change meta description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,13 +4,13 @@
 	import Cube from '../components/Cube.svelte';
 </script>
 
-<head>
+<svelte:head>
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title>Raahim Zee</title>
 	<meta name="description" content="This page is the personal website of Raahim Zee" />
-</head>
+</svelte:head>
 
 <main class="">
 	<slot />
