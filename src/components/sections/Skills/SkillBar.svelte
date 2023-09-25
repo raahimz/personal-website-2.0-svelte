c69bf2ea6f21b8dbc73bd4236c9572032442ae3f
Raahim Zeeshan <raahim.home@gmail.com>
2023-09-26 03:39:10 +0500

    update color

--- src/components/sections/Skills/SkillBar.svelte
+++ src/components/sections/Skills/SkillBar.svelte
@@ -9,7 +9,7 @@
 	</div>
 	<div class="md:w-52 w-72 bg-gray-200 border-2 border-gray-300 rounded-full">
 		<div
-			class="h-4 bg-gradient-to-r from-gray-800 to-gray-500 rounded-full border-2 shadow-sm shadow-gray-500 border-gray-100"
+			class="h-4 bg-gradient-to-r from-black to-gray-700 rounded-full border-2 shadow-sm shadow-gray-500 border-gray-100"
 			style="width: {percentage}%;"
 		/>
 	</div>
--- src/components/sections/Skills/SkillCard.svelte
+++ src/components/sections/Skills/SkillCard.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="bg-black rounded-md shadow-md shadow-gray-800 w-fit p-2 flex flex-col gap-2 border-2 border-gray-300"
+	class="bg-black rounded-md shadow-sm shadow-gray-600 w-fit p-2 flex flex-col gap-2 border-2 border-gray-300"
 >
 	<div class="bg-white rounded-md py-1 px-3 shadow-black shadow-sm border-2 border-gray-300">
 		<h1 class="text-2xl font-mono text-center font-bold text-black">{title.toUpperCase()}</h1>
